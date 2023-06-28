674945deefc85ed5094a445729c6e0c887f75e90
kyem-asa <turtlemoons@protonmail.com>
2023-06-28 09:44:29 -0500

    add profile components

--- src/index.js
+++ src/index.js
@@ -2,15 +2,47 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
 
+function Avatar() {
+  return (
+    <div>
+      <img
+        src="pinkclouds.jpg"
+        alt="pink clouds in the sky"
+        className="image"
+      />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>kyem asa</h1>
+      <p>
+        Full-stack web developer and Genshin Impact player based in Texas. When
+        not coding, I'm playing video games or reading manwha. Feel to reach out
+        to me to get in touch!
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skillList">
+      <p className="skill">HTML & CSS</p>
+      <p>UX/UI design</p>
+      <p></p>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="card">
       <Avatar />
       <div className="data">
         <Intro />
-        {/* Should contain one Skill component
-        for each web dev skill that you have,
-        customized with props */}
         <SkillList />
       </div>
     </div>
